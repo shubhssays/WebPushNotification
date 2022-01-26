388755eb1fee4da437cd7eea0f2226ca0d60e5b4
unknown <nmimsportalteam@gmail.com>
2022-01-26 14:16:32 +0530

    Basic web notifications setup completed

---
+++ BackEnd/server.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require('express');
+const webPush = require('web-push');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const publicVapidKey = process.env.PUBLIC_VAPID_KEY;
+const privateVapidKey = process.env.PRIVATE_VAPID_KEY;
+
+webPush.setVapidDetails('mailto:shubhssays@gmail.com', publicVapidKey, privateVapidKey);
+
+app.get('/', (req, res) =>{
+    res.render('index.ejs')
+})
+
+app.post('/subscribe', (req, res) => {
+  const subscription = req.body
+
+  res.status(201).json({});
+
+  const payload = JSON.stringify({
+    title: 'Push notifications with Service Workers',
+  });
+
+  webPush.sendNotification(subscription, payload)
+    .catch(error => console.error(error));
+});
+
+app.set('port', process.env.PORT || 5000);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
